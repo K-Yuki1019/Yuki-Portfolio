4236eb91c6e3e2b9e268d12b493b56adb86b4ead
Yuki Kumamoto <y.kumamoto2021@gmail.com>
2021-07-02 19:12:59 +0900

    debug(card.ts): strictPropertyInitializationのデバッグ

--- src/app/works/card/card.component.ts
+++ src/app/works/card/card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from 'src/app/interfaces/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  @Input() article!: Article;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.article == null) {
+      throw new Error('[article] is required');
+    }
+    this.someFunc(this.article);
   }
 
+  someFunc(article: Article) {}
 }
