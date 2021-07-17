371b51025bb8f6fd143b47dc3ee4fcb4dcd158cb
K-Yuki1019 <63516648+K-Yuki1019@users.noreply.github.com>
2021-07-18 01:18:48 +0900

    fix(index): Index画面とデータを紐付け/ マークアップ修正

--- src/app/admin/index/index.component.ts
+++ src/app/admin/index/index.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
+  afUser$ = this.authService.afUser$;
+  articles$: Observable<Article[]> = this.articleService.getArticles();
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private articleService: ArticleService
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
